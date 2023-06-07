c71414c1c98f8c9d49f5679197c3c9d00c676545
ST359 <135271126+ST359@users.noreply.github.com>
2023-06-07 03:26:47 +0300

    Remove path to ffprobe and ffmpeg

--- video_convertion.py
+++ video_convertion.py
@@ -1,12 +1,10 @@
 import ffmpeg
 from converter import Converter
 import platform
-path_to_ffmpeg = r'C:\ffmpeg\bin\ffmpeg.exe'
-path_to_ffprobe = r'C:\ffmpeg\bin\ffprobe.exe'
 
 
 def convert_video(path_to_file: str):
-    c = Converter(path_to_ffmpeg, path_to_ffprobe)
+    c = Converter()
     info = c.probe(path_to_file)
     if platform.system() == 'Windows':
         conv = c.convert(path_to_file, 'converted_vids/output.mp4', {
