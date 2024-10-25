f0e8e20b7c4e79e5177b4aec9ea44b7aa09c848c
ST359 <135271126+ST359@users.noreply.github.com>
2024-10-25 14:55:07 +0300

    feat: add try-catch to async ddinsta check

--- message_url_processing.py
+++ message_url_processing.py
@@ -3,7 +3,6 @@ import re
 import requests
 import httpx
 
-
 regex_post_not_found = r"(content=\"Post not found\"|content=\"Post might not be available\")"
 regex_not_spoiler = r"(https?://(?:www\.)?(?:twitter\.com|instagram\.com|x\.com)/[^\|\s]+)"
 regex_not_spoiler_ddinsta = r"(https?://(?:www\.)?(?:ddinstagram\.com)/[^\|\s]+)"
@@ -20,10 +19,13 @@ def extract_url_from_message(message: str) -> tuple:
     parsed_urls = [urllib.parse.urlparse(url) for url in raw_urls]
     return raw_urls, parsed_urls
 
+
 def does_contain_urls(message: str) -> bool:
     if len(extract_url_from_message(message)[0]) > 0:
         return True
     return False
+
+
 def is_ddinsta_url_working(url: str) -> bool:
     try:
         r = requests.get(url).text
@@ -32,13 +34,19 @@ def is_ddinsta_url_working(url: str) -> bool:
     except:
         return False
     return True
+
+
 async def is_ddinsta_url_working_async(url: str) -> bool:
     async with httpx.AsyncClient() as client:
-        r = await client.get(url)
-        if re.search(regex_post_not_found, r.text):
+        try:
+            r = await client.get(url)
+            if re.search(regex_post_not_found, r.text) or r.status_code != 200:
+                return False
+            else:
+                return True
+        except:
             return False
-        else:
-            return True
+
 
 async def make_url_embeddable(url_in: list) -> list:
     urls_out = []
@@ -52,9 +60,10 @@ async def make_url_embeddable(url_in: list) -> list:
             if is_working:
                 urls_out.append(urllib.parse.urlunparse(new_url))
             else:
-                new_url = url._replace(netloc =instagram_url_embeddable_backup)
+                new_url = url._replace(netloc=instagram_url_embeddable_backup)
                 urls_out.append(urllib.parse.urlunparse(new_url))
     return urls_out
+
 
 def replace_urls_with_embeddables(message_in: str, raw_urls: list, embeddable_urls: list) -> str:
     new_message = message_in
