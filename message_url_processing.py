3e8d2d0f954c17f983748843e639a36288f7130c
ST359 <135271126+ST359@users.noreply.github.com>
2025-02-20 23:11:10 +0300

    feat: implement ebmedding of shared instagram links

--- message_url_processing.py
+++ message_url_processing.py
@@ -2,6 +2,7 @@ import urllib.parse
 import re
 import requests
 import httpx
+from bs4 import BeautifulSoup
 
 regex_post_not_found = r"(content=\"Post not found\"|content=\"Post might not be available\")"
 regex_not_spoiler = r"(https?://(?:www\.)?(?:twitter\.com|instagram\.com|x\.com)/[^\|\s]+)"
@@ -55,6 +56,10 @@ async def make_url_embeddable(url_in: list) -> list:
             new_url = url._replace(netloc=twitter_url_embeddable)
             urls_out.append(urllib.parse.urlunparse(new_url))
         elif url.netloc in instagram_url:
+            if "share" in url.path:
+                final_id = get_final_reel_id(urllib.parse.urlunparse(url))
+                final_path = "/reel/"+final_id
+                url = url._replace(path=final_path)
             new_url = url._replace(netloc=instagram_url_embeddable)
             is_working = await is_ddinsta_url_working_async(urllib.parse.urlunparse(new_url))
             if is_working:
@@ -70,3 +75,14 @@ def replace_urls_with_embeddables(message_in: str, raw_urls: list, embeddable_ur
     for old_url, new_url in zip(raw_urls, embeddable_urls):
         new_message = new_message.replace(old_url, new_url)
     return new_message
+
+def get_final_reel_id(share_url) ->str:
+    response = requests.get(share_url)
+
+    if response.status_code == 200:
+        final_url = response.url
+        reel_id = final_url.split('/')[-2]  # The reel ID is the second to last component
+        return reel_id
+    else:
+        print(f"Failed to fetch page: {response.status_code}")
+        return ""
